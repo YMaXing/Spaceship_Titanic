c82dec1bfacd4bba7eb670baf2bfe4e6ce907d3e
yxing <garrusxing@gmail.com>
2024-04-09 11:08:07 +0200

    Updated data version from 0 to v1

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -27,9 +27,8 @@ def initialize_dvc_storage(dvc_remote_name: str, dvc_remote_url: str) -> None:
     if not run_shell_command("dvc remote list"):
         DATA_UTILS_LOGGER.info("Initializing DVC storage...")
         run_shell_command(f"dvc remote add -d {dvc_remote_name} {dvc_remote_url}")
-        run_shell_command("git config --global --add safe.directory /app")
         run_shell_command("git add .dvc/config")
-        run_shell_command("git commit -nm 'Initialized dvc remote storage at {dvc_remote_url}'")
+        run_shell_command("git commit -m 'Initialized dvc remote storage at {dvc_remote_url}'")
     else:
         DATA_UTILS_LOGGER.info("DVC storage is already initialized...")
 
@@ -40,9 +39,8 @@ def commit_to_dvc(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
         current_version = "0"
     next_version = f"v{int(current_version) + 1}"
     run_shell_command(f"dvc add {dvc_raw_data_folder}")
-    run_shell_command("git config --global --add safe.directory /app")
     run_shell_command("git add .")
-    run_shell_command(f"git commit -nm 'Updated data version from {current_version} to {next_version}'")
+    run_shell_command(f"git commit -m 'Updated data version from {current_version} to {next_version}'")
     run_shell_command(f"git tag -a {next_version} -m 'Data version {next_version}'")
     run_shell_command(f"dvc push -r {dvc_remote_name}")
     run_shell_command("git push --follow-tags")
