e8220e2698bc19500307df8d71dae6ec6d76a4d0
YMaXing <garrusxing@gmail.com>
2024-04-13 09:15:08 +0200

    more libraries added for the infrastructure

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,6 +1,7 @@
-from subprocess import CalledProcessError
-from src.utils.utils import get_logger, run_shell_command
 from pathlib import Path
+from subprocess import CalledProcessError
+
+from src.utils.utils import get_logger, run_shell_command
 
 DATA_UTILS_LOGGER = get_logger(Path(__file__).name)
 
